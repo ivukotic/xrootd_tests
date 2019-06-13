d6c1e9c06a5a09f862c0fc7ad026226cb406cf38
Ilija Vukotic <ivukotic@cern.ch>
2019-06-13 15:43:06 -0500

    add temp

---
+++ kube/es_template.sh
@@ -0,0 +1,40 @@
+{
+  "testing_xrootd" : {
+    "order" : 0,
+    "index_patterns" : [
+      "testing_xrootd"
+    ],
+    "settings" : {
+      "index" : {
+        "number_of_shards" : "2",
+        "number_of_replicas" : "1"
+      }
+    },
+    "mappings" : {
+      "data" : {
+        "properties" : {
+          "endpoint" : {
+            "type" : "keyword"
+          },
+          "issues" : {
+            "type" : "keyword"
+          },
+          "path" : {
+            "type" : "keyword"
+          },
+          "site" : {
+            "type" : "keyword"
+          },
+          "state" : {
+            "type" : "keyword"
+          },
+          "timestamp" : {
+            "type" : "date",
+            "format" : "basic_date_time_no_millis"
+          }
+        }
+      }
+    },
+    "aliases" : { }
+  }
+}
