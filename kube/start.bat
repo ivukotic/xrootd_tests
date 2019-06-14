daaf45f4e6ff709971a17cc5ab9882948c9d3058
Ilija Vukotic <ivukotic@cern.ch>
2019-06-14 12:18:42 -0500

    fd

--- kube/start.bat
+++ kube/start.bat
@@ -1,5 +1,7 @@
 kubectl create namespace aaas
 
+kubectl create -f secrets/ES-secret.yaml
+
 echo "Adding x509 cert needed for data access"
 kubectl delete secret -n aaas x509-secret
 kubectl create secret -n aaas generic x509-secret --from-file=userkey=secrets/xcache.key.pem --from-file=usercert=secrets/xcache.crt.pem
