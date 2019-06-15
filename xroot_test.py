982de8445a8775aa0ae6b38ca3db169ae9e36fad
Ilija Vukotic <ivukotic@cern.ch>
2019-06-15 16:46:44 -0500

    small

--- xroot_test.py
+++ xroot_test.py
@@ -1,51 +1,53 @@
 #!/usr/bin/env python
-import sys, os, time
-from  datetime import datetime
+import sys
+import os
+import time
+from datetime import datetime
 import tools
 
 if os.environ.get("RUCIO_ACCOUNT") != None:
-    rucio_account=os.environ.get("RUCIO_ACCOUNT")
+    rucio_account = os.environ.get("RUCIO_ACCOUNT")
 else:
     print("no RUCIO_ACCOUNT environment found. Please set it before using this program.")
-    sys.exit(1) 
+    sys.exit(1)
 
 endpoints = tools.getXROOTendpoints()
 ddms = tools.getDDMendpoints()
 
-reps=tools.find_replicas('mc15_13TeV','HITS.06828093._000096.pool.root.1')
+reps = tools.find_replicas('mc15_13TeV', 'HITS.06828093._000096.pool.root.1')
 
-docs=[]
+docs = []
 for ep in endpoints:
     print('------------------------------------------------------------')
     site = ep['site']
-    doc={'_index':'testing_xrootd', '_type':'rucio_paths', 'site':site, 'issues':[], 'endpoint':ep['name']}
-    print('Site:',site, 'doortype:', ep['door_type'], 'address:', ep['address'])
-    
-    if ep['state'] != 'ACTIVE': 
+    doc = {'_index': 'testing_xrootd', '_type': 'docs', 'site': site, 'issues': [], 'endpoint': ep['name']}
+    print('Site:', site, 'doortype:', ep['door_type'], 'address:', ep['address'])
+
+    if ep['state'] != 'ACTIVE':
         doc['issues'].append('xrootd door not active')
-    if ep['door_type']=='':
-        doc['issues'].append('door type not set' )
-    if ep['door_type']=='internal' or ep['door_type']=='proxyinternal':
+    if ep['door_type'] == '':
+        doc['issues'].append('door type not set')
+    if ep['door_type'] == 'internal' or ep['door_type'] == 'proxyinternal':
         doc['issues'].append("can't test this door")
 
-    sites_ddms=ddms[site]
+    sites_ddms = ddms[site]
     # print(sites_ddms)
 
     found = False
     for ddm in sites_ddms:
         if ddm in reps:
-            fp=reps[ddm]
+            fp = reps[ddm]
             if not fp.startswith(ep['address']):
                 doc['issues'].append('rucio returns different door: ' + fp)
                 break
                 #fp = ep['address'] + fp[fp.rfind('//'):]
             print('to check: ', fp)
             doc['path'] = fp
-            found=True
+            found = True
             break
-    if not found: 
+    if not found:
         doc['issues'].append("no replica at the site")
-        
+
     if len(doc['issues']):
         print(doc['issues'])
 
@@ -56,20 +58,20 @@ for d in docs:
 print('Done.')
 
 
-        
 print 'creating script to execute'
-    
+
 ts = datetime.utcnow().strftime("%Y%m%dT%H0000Z")
 logpostfix = '_' + ts + '.log'
 redstring = ' - 2>&1 >/dev/null | cat >'
 
-try:    
-    with open('checkDirect.sh', 'w') as f: 
+try:
+    with open('checkDirect.sh', 'w') as f:
         for d in docs:
             d['timestamp'] = ts
-            if 'path' not in d: continue
+            if 'path' not in d:
+                continue
             logfile = d['endpoint'] + logpostfix
-            cpcomm='timeout 270 xrdcp -d 2 -f -np '
+            cpcomm = 'timeout 270 xrdcp -d 2 -f -np '
             comm = cpcomm + d['path'] + redstring + logfile + ' & \n'
             f.write('echo "command executed:\n ' + comm + '" >> ' + logfile + '\n')
             f.write('echo "========================================================================" >> ' + logfile + '\n')
@@ -77,9 +79,9 @@ try:
         f.close()
 except:
     print "Unexpected error:", sys.exc_info()[0]
-#sys.exit(0)
+# sys.exit(0)
 print 'executing all of the xrdcps in parallel. 5 min timeout.'
-com = tools.Command("source ./checkDirect.sh")    
+com = tools.Command("source ./checkDirect.sh")
 com.run(300)
 time.sleep(250)
 
@@ -88,18 +90,19 @@ print 'checking log files'
 
 # checking which sites gave their own file directly
 for d in docs:  # this is file to be asked for
-    if 'path' not in d: continue
+    if 'path' not in d:
+        continue
     logfile = d['endpoint'] + logpostfix
     try:
         with open(logfile, 'r') as f:
-            lines=f.readlines()
-            succ=False
+            lines = f.readlines()
+            succ = False
             for l in lines:
                 # print l
                 if tools.isMsgOK(l):
-                    succ=True
+                    succ = True
                     break
-            if succ==True:
+            if succ == True:
                 print logfile, "works"
                 d['state'] = "Works"
             else:
